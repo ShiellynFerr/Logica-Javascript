ff83c57ae7e445ac5d41cfcc981414d0859e1aed
Shiellyn <brunashiellyn@gmail.com>
2022-04-27 17:14:24 -0300

    calcula imc

--- logica/imc/script.js
+++ logica/imc/script.js
@@ -2,6 +2,7 @@
 function pulaLinha() {
 
     document.write("<br>")
+    document.write("<br>")
 }
 
 function mostra(mensagem) {
@@ -13,9 +14,17 @@ function mostra(mensagem) {
 
 
  function calculaImc (peso, altura) {
-    let imc = peso / (altura * altura);
-    mostra('O imc calculado é ' + imc);
- }
+    return peso / (altura * altura);
 
- calculaImc(47, 1.57);
- calculaImc(80, 1.90);
+ }
+ let totalImc = calculaImc(47, 1.57) + calculaImc(80, 1.90);
+ document.write("A soma dos Imc's é " + totalImc);
+
+ let nome = prompt('Informe o seu nome: ')
+ let alturaInformada = prompt( nome + ' Qual é a sua altura?');
+ let pesoInformado = prompt( nome + ' Qual é o seu peso?');
+
+ pulaLinha()
+
+ let imc = calculaImc(pesoInformado, alturaInformada);
+ document.write('O imc calculado é: ' + imc)
